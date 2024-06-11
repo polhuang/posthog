216ae219a385dfdde96069850e9c6c0df6696bea
polhuang <paulleehuang@gmail.com>
2024-06-11 05:07:40 -0500

    Fixed type error in PostHog snippet

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,12 +7,19 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY
+  const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST
+  if (posthogKey && posthogHost) {
+    posthog.init(posthogKey, {
+      api_host: posthogHost,
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
   })
+  } else {
+    console.error('No PostHog key or host')
+  }z
 }
-export function CSPostHogProvider({ children }) {
+
+export function CSPostHogProvider({ children }:  { children: ReactNode}) {
     return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
 
